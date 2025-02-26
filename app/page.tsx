c1acaa6689740232e7b642ae7b00efeaf51617a9
shakthishamruth <shakthishamruth@gmail.com>
2025-02-27 02:08:07 +0530

    update dialog box

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { api } from "@/convex/_generated/api";
 import { SignedOut, useOrganization, useUser } from "@clerk/clerk-react";
 import { SignedIn, SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z.
+    custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, `Required`),
+})
 
 export default function Home() {
 
@@ -13,6 +36,22 @@ export default function Home() {
 
   const organization = useOrganization();
   const user = useUser();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  })
+
+  const fileRef = form.register("file");
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // 
+    console.log(values);
+    console.log(values.file);
+  }
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -25,26 +64,76 @@ export default function Home() {
   const createFile = useMutation(api.files.createFile);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        {files?.map(file => {
-          return <div key={file._id}>{file.name}</div>
-        })}
+    <main className="container mx-auto pt-12">
 
-        <Button onClick={() => {
-          if (!orgId) return;
-          createFile(
-            {
-              name: "hello world",
-              orgId,
-            }
-          )
-        }}>
-          click me
-        </Button>
+      <div className="flex justify-between items-center">
+        <h1 className="text-4 font-bold">
+          Your Files
+        </h1>
 
-      </main>
-    </div>
+
+        <Dialog>
+          <DialogTrigger asChild><Button onClick={() => {
+            if (!orgId) return;
+            createFile(
+              {
+                name: "hello world",
+                orgId,
+              }
+            )
+          }}>
+            Upload
+          </Button></DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your file here</DialogTitle>
+              <DialogDescription>
+
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={({ }) => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+
+      </div>
+
+      {files?.map(file => {
+        return <div key={file._id}>{file.name}</div>
+      })}
+
+    </main>
   );
 }
