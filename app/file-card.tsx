33d4a8a6df81776b9115aeb0152c798b94b36769
shakthishamruth <shakthishamruth@gmail.com>
2025-03-29 00:45:28 +0530

    Update Delete

--- app/file-card.tsx
+++ app/file-card.tsx
@@ -8,12 +8,77 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Doc } from "@/convex/_generated/dataModel"
+import { toast } from "sonner"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DeleteIcon, MoreVertical, TrashIcon } from "lucide-react"
+import { useState } from "react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+
+function FileCardActions({ file }: { file: Doc<"files"> }) {
+    const deleteFile = useMutation(api.files.deleteFile);
+    const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+    return (
+        <>
+            <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your file.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={async () => {
+                            // TODO: del file
+                            await deleteFile({
+                                fileId: file._id,
+                            });
+                            toast("File Deleted!");
+                        }}>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger><MoreVertical className="w-6 h-6" /></DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setIsConfirmOpen(true)} className="flex gap-1 text-red-600 items-center cursor-pointer">
+                        <TrashIcon className="w-4 h-4" /> Delete </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
 
 export function FileCard({ file }: { file: Doc<"files"> }) {
     return (
         <Card>
-            <CardHeader>
+            <CardHeader className="relative">
                 <CardTitle>{file.name}</CardTitle>
+                <div className="absolute top-1 right-1">
+                    <FileCardActions file={file} />
+                </div>
                 {/*<CardDescription>Card Description</CardDescription>*/}
             </CardHeader>
 
@@ -23,7 +88,6 @@ export function FileCard({ file }: { file: Doc<"files"> }) {
 
             <CardFooter>
                 <Button className="mr-4"> Download </Button>
-                <Button> Delete </Button>
             </CardFooter>
         </Card>
     )
--- convex/files.ts
+++ convex/files.ts
@@ -74,3 +74,28 @@ export const getFiles = query({
         ).collect();
     }
 });
+
+export const deleteFile = mutation({
+    args: { fileId: v.id("files") },
+    async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new ConvexError("You do not have access to this organisation");
+        }
+
+        const file = await ctx.db.get(args.fileId);
+
+        if (!file) {
+            throw new ConvexError("This file does not exist!");
+        }
+
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, file.orgId);
+
+        if (!hasAccess) {
+            throw new ConvexError("You do not have access to delete this file!");
+        }
+
+        await ctx.db.delete(args.fileId);
+    }
+})
